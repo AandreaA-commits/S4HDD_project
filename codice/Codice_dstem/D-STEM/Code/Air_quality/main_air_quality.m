fc5de997f2d8eee4108906f8db5ebbc377ad8382
MarchesiGabriele <marchesigabriele2819@gmail.com>
2023-12-07 17:30:45 +0100

    COCK

--- codice/Codice_dstem/D-STEM/Code/Air_quality/main_air_quality.m
+++ codice/Codice_dstem/D-STEM/Code/Air_quality/main_air_quality.m
@@ -1,5 +1,5 @@
-%clc
-%clearvars
+clc
+clearvars
 
 %{
 repositoryPath = fullfile(pwd, 'GitHub\S4HDD_project\codice');
@@ -13,25 +13,275 @@ load(percorsoCompleto);
 %}
 
 %% setup dei dati
-addpath('C:\Users\arici\Documents\GitHub\S4HDD_project\codice\Codice_dstem\D-STEM\Src'); %D-STEM
+%addpath('C:\Users\arici\Documents\GitHub\S4HDD_project\codice\Codice_dstem\D-STEM\Src'); %D-STEM
+addpath('../../Src'); %D-STEM
+a = load("..\..\..\..\dati_pollutants_e_scraper\dati_pollutants_2019.mat")
+dati_pollutants_2019 = a.dati_meteo_2019;
+load ..\..\..\..\dati_traffico_2019.mat
+load ..\..\..\..\dati_meteo_2019.mat
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %      Data  building     %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-NO2 = dati_pollutants_2019(4, 2:end);
-dati_NO2 = [];
+NOX = dati_pollutants_2019(7, 2:end);
+PM25 = dati_pollutants_2019(5, 2:end);
+BEN = dati_pollutants_2019(10, 2:end);
 
-for i = 1:size(NO2, 2)
-    tabella_corrente = NO2{1, i};
+VEL_VENTO = dati_meteo_2019(2, 2:end)
+PRECIPITAZIONI = dati_meteo_2019(8, 2:end)
+UMIDITA = dati_meteo_2019(5, 2:end)
+
+
+%% RIMOZIONE STAZIONI CHE NON HANNO MISURAZIONI IN TUTTI I MESI DELLA ANNO
+% PM25
+min_col = size(PM25{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(PM25{1,iii},1)
+        min_col = size(PM25{1,iii},1);
+        col = iii;
+    end
+end
+
+stazioni_comuni = unique(PM25{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(PM25{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(PM25{1,mese}, 1)
+        if ~ismember(PM25{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; PM25{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(PM25{1,mese}(:,1)) == table2array(temp(:,1)));
+        PM25{1,mese} = PM25{1,mese}(rowremove, :);       
+        disp("COCK")
+    end
+end
+% NOX
+min_col = size(NOX{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(NOX{1,iii},1)
+        min_col = size(NOX{1,iii},1);
+        col = iii;
+    end
+end
+
+stazioni_comuni = unique(NOX{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(NOX{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(NOX{1,mese}, 1)
+        if ~ismember(NOX{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; NOX{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(NOX{1,mese}(:,1)) == table2array(temp(:,1)));
+        NOX{1,mese} = NOX{1,mese}(rowremove, :);       
+        disp("COCK")
+    end
+end
+% BEN
+min_col = size(BEN{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(BEN{1,iii},1)
+        min_col = size(BEN{1,iii},1);
+        col = iii;
+    end
+end
+
+stazioni_comuni = unique(BEN{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(BEN{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(BEN{1,mese}, 1)
+        if ~ismember(BEN{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; BEN{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(BEN{1,mese}(:,1)) == table2array(temp(:,1)));
+        BEN{1,mese} = BEN{1,mese}(rowremove, :);       
+        disp("COCK")
+    end
+end
+
+% VEL VENTO
+min_col = size(VEL_VENTO{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(VEL_VENTO{1,iii},1)
+        min_col = size(VEL_VENTO{1,iii},1);
+        col = iii;
+    end
+end
+
+stazioni_comuni = unique(VEL_VENTO{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(VEL_VENTO{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(VEL_VENTO{1,mese}, 1)
+        if ~ismember(VEL_VENTO{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; VEL_VENTO{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(VEL_VENTO{1,mese}(:,1)) == table2array(temp(:,1)));
+        VEL_VENTO{1,mese} = VEL_VENTO{1,mese}(rowremove, :);       
+        disp("COCK")
+    end
+end
+
+% PRECIPITAZIONI
+min_col = size(PRECIPITAZIONI{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(PRECIPITAZIONI{1,iii},1)
+        min_col = size(PRECIPITAZIONI{1,iii},1);
+        col = iii;
+    end
+end
+
+stazioni_comuni = unique(PRECIPITAZIONI{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(PRECIPITAZIONI{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(PRECIPITAZIONI{1,mese}, 1)
+        if ~ismember(PRECIPITAZIONI{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; PRECIPITAZIONI{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(PRECIPITAZIONI{1,mese}(:,1)) == table2array(temp(1,1)));
+        PRECIPITAZIONI{1,mese} = PRECIPITAZIONI{1,mese}(rowremove, :); 
+        rowremove
+        disp("COCK")       
+    end
+end
+min_col = size(PRECIPITAZIONI{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(PRECIPITAZIONI{1,iii},1)
+        min_col = size(PRECIPITAZIONI{1,iii},1);
+        col = iii;
+    end
+end
+stazioni_comuni = unique(PRECIPITAZIONI{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(PRECIPITAZIONI{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(PRECIPITAZIONI{1,mese}, 1)
+        if ~ismember(PRECIPITAZIONI{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; PRECIPITAZIONI{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(PRECIPITAZIONI{1,mese}(:,1)) == table2array(temp(1,1)));
+        PRECIPITAZIONI{1,mese} = PRECIPITAZIONI{1,mese}(rowremove, :); 
+        rowremove
+        disp("COCK")       
+    end
+end
+
+% UMIDITA
+min_col = size(UMIDITA{1,1},1);
+col = 1;
+for iii = 2:12
+    if min_col > size(UMIDITA{1,iii},1)
+        min_col = size(UMIDITA{1,iii},1);
+        col = iii;
+    end
+end
+
+stazioni_comuni = unique(UMIDITA{1,col}(:,1));
+for mese = 1:12
+    stazioni_mese = unique(UMIDITA{1,mese}(:,1));
+    temp = [];
+    for row = 1: size(UMIDITA{1,mese}, 1)
+        if ~ismember(UMIDITA{1,mese}(row,1), stazioni_comuni)
+            disp("COCCO")
+            temp = [temp; UMIDITA{1,mese}(row, :)];
+        end
+    end
+    if ~size(temp,1) == 0         
+        rowremove = ~(table2array(UMIDITA{1,mese}(:,1)) == table2array(temp(:,1)));
+        UMIDITA{1,mese} = UMIDITA{1,mese}(rowremove, :);       
+        disp("COCK")
+    end
+end
+
+
+
+
+
+dati_NOX = [];
+dati_PM25 = [];
+dati_BEN = [];
+dati_VELVENTO = [];
+dati_PRECIPITAZIONI = [];
+dati_UMIDITA = [];
+
+
+for i = 1:size(NOX, 2)
+    % NOX
+    tabella_corrente = NOX{1, i};
     colonne_numeriche = tabella_corrente{:, 5:end};
-    dati_NO2 = [dati_NO2 colonne_numeriche];
+    dati_NOX = [dati_NOX colonne_numeriche];
+    % pm2.5
+    tabella_corrente = PM25{1, i};
+    colonne_numeriche = tabella_corrente{:, 5:end};
+    dati_PM25 = [dati_PM25 colonne_numeriche];
+    % BEN
+    tabella_corrente = BEN{1, i};
+    colonne_numeriche = tabella_corrente{:, 5:end};
+    dati_BEN = [dati_BEN colonne_numeriche];
+    % VEL VENTO
+    tabella_corrente = VEL_VENTO{1, i};
+    colonne_numeriche = tabella_corrente{:, 4:end};
+    dati_VELVENTO = [dati_VELVENTO colonne_numeriche];
+    % PRECIPITAZIONI
+    tabella_corrente = PRECIPITAZIONI{1, i};
+    colonne_numeriche = tabella_corrente{:, 4:end};
+    dati_PRECIPITAZIONI = [dati_PRECIPITAZIONI colonne_numeriche];
+    % UMIDITA
+    tabella_corrente = UMIDITA{1, i};
+    colonne_numeriche = tabella_corrente{:, 4:end};
+    dati_UMIDITA = [dati_UMIDITA colonne_numeriche];
 end
 
 traffico = str2double(totale(:, 4:end));
 
+% creazione vettore covariata is_weekend
+% 1 gennaio 2019 era martedì
+is_weekend = zeros(1,365*24);
+flag_weekend = 48;
+flag_week = 24*5;
+for i = 4*24+1:size(is_weekend,2) % itero tutte le ore dell'anno 
+    if flag_weekend ~= 0
+        is_weekend(1,i) = 1;
+        flag_weekend = flag_weekend -1; 
+        flag_week = 24*5;
+    else
+        if flag_week == 0
+            flag_weekend = 48;
+        else
+            flag_week = flag_week-1; 
+        end
+    end    
+end
+
 %load no2 obs
-ground.Y{1} = dati_NO2;
+ground.Y{1} = dati_NOX;
 ground.Y_name{1} = 'no2';
 n1 = size(ground.Y{1}, 1);
 T = size(ground.Y{1}, 2);
