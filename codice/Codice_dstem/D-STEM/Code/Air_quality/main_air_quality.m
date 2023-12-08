a38213dec9fcef7ef33ef295c26a6a93c057f9db
your_username <a.arici4@studenti.unibg.it>
2023-12-08 11:37:24 +0100

    sad commit

--- codice/Codice_dstem/D-STEM/Code/Air_quality/main_air_quality.m
+++ codice/Codice_dstem/D-STEM/Code/Air_quality/main_air_quality.m
@@ -15,7 +15,7 @@ load(percorsoCompleto);
 %% setup dei dati
 %addpath('C:\Users\arici\Documents\GitHub\S4HDD_project\codice\Codice_dstem\D-STEM\Src'); %D-STEM
 addpath('../../Src'); %D-STEM
-a = load("..\..\..\..\dati_pollutants_e_scraper\dati_pollutants_2019.mat")
+a = load("..\..\..\..\dati_pollutants_e_scraper\dati_pollutants_2019.mat");
 dati_pollutants_2019 = a.dati_meteo_2019;
 load ..\..\..\..\dati_traffico_2019.mat
 load ..\..\..\..\dati_meteo_2019.mat
@@ -282,7 +282,7 @@ end
 
 %load no2 obs
 ground.Y{1} = dati_NOX;
-ground.Y_name{1} = 'no2';
+ground.Y_name{1} = 'nox';
 n1 = size(ground.Y{1}, 1);
 T = size(ground.Y{1}, 2);
 
@@ -291,14 +291,135 @@ ground.Y{2} = traffico;
 ground.Y_name{2} = 'traffic';
 n2 = size(ground.Y{2}, 1);
 
+%load pm2.5 obs
+ground.Y{3} = dati_PM25;
+ground.Y_name{3} = 'pm25';
+n3 = size(ground.Y{3}, 1);
 
-%load covariates for the NO2 monitoring stations (ci mettiamo l'altezza delle stazioni)
-% devo prendere solo la colonna della elevation
-dati_elevation = tabella_corrente{:, 2};
+%load benzene obs
+ground.Y{4} = dati_BEN;
+ground.Y_name{4} = 'ben';
+n4 = size(ground.Y{4}, 1);
 
-ground.X_beta{1} = dati_elevation;
-ground.X_beta_name{1} = {'elevation'};
-ground.X_beta{1} = ones();
+%load umidity obs
+ground.Y{5} = dati_UMIDITA;
+ground.Y_name{5} = 'umidity';
+n5 = size(ground.Y{5}, 1);
+
+%load precipitation obs
+ground.Y{6} = dati_PRECIPITAZIONI;
+ground.Y_name{6} = 'prec';
+n6 = size(ground.Y{6}, 1);
+
+%load wind velocity obs
+ground.Y{7} = dati_VELVENTO;
+ground.Y_name{7} = 'wind_vel';
+n7 = size(ground.Y{7}, 1);
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n1, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n1,1);
+    else
+        X(:,1,i) = ones(n1,1);
+    end
+end
+
+
+%load of sunday flags
+ground.X_beta{1} = X;
+ground.X_beta_name{1} = {'weekend'};
+
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n2, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n2,1);
+    else
+        X(:,1,i) = ones(n2,1);
+    end
+end
+
+
+ground.X_beta{2} = X;
+ground.X_beta_name{2} = {'weekend'};
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n3, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n3,1);
+    else
+        X(:,1,i) = ones(n3,1);
+    end
+end
+
+
+ground.X_beta{3} = X;
+ground.X_beta_name{3} = {'weekend'};
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n4, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n4,1);
+    else
+        X(:,1,i) = ones(n4,1);
+    end
+end
+
+
+ground.X_beta{4} = X;
+ground.X_beta_name{4} = {'weekend'};
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n5, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n5,1);
+    else
+        X(:,1,i) = ones(n5,1);
+    end
+end
+
+ground.X_beta{5} = X;
+ground.X_beta_name{5} = {'weekend'};
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n6, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n6,1);
+    else
+        X(:,1,i) = ones(n6,1);
+    end
+end
+
+ground.X_beta{6} = X;
+ground.X_beta_name{6} = {'weekend'};
+
+%matrice [stazioni x numero_covariate x giorni]
+X = zeros(n7, 1, T);
+for i=1:size(is_weekend,2)
+    if is_weekend(i) == 0
+        %creiamo una matrice n_stazioni x 1
+        X(:,1,i) = zeros(n7,1);
+    else
+        X(:,1,i) = ones(n7,1);
+    end
+end
+
+ground.X_beta{7} = X;
+ground.X_beta_name{7} = {'weekend'};
+
 
 %X_z
 ground.X_z{1} = ones(n1, 1);
@@ -307,26 +428,72 @@ ground.X_z_name{1} = {'constant'};
 ground.X_z{2} = ones(n2, 1);
 ground.X_z_name{2} = {'constant'};
 
+ground.X_z{3} = ones(n3, 1);
+ground.X_z_name{3} = {'constant'};
+
+ground.X_z{4} = ones(n4, 1);
+ground.X_z_name{4} = {'constant'};
+
+ground.X_z{5} = ones(n5, 1);
+ground.X_z_name{5} = {'constant'};
+
+ground.X_z{6} = ones(n6, 1);
+ground.X_z_name{6} = {'constant'};
+
+ground.X_z{7} = ones(n7, 1);
+ground.X_z_name{7} = {'constant'};
+
 obj_stem_varset_p = stem_varset(ground.Y, ground.Y_name, [], [], ...
-                                [], [], ... 
+                                ground.X_beta, ground.X_beta_name, ... 
                                 ground.X_z, ground.X_z_name);
 
-
-NO2_lat = tabella_corrente{:,3};
-NO2_long = tabella_corrente{:,4};
+%laod of the station coordinates
+NOx_lat = NOX{1,1}{:,3};
+NO2_long = NOX{1,1}{:,4};
 
 traffic_lat = str2double(totale(:,2));
 traffic_long = str2double(totale(:,3));
+
+PM25_lat = PM25{1,1}{:,3};
+PM25_long = PM25{1,1}{:,4};
+
+BEN_lat = BEN{1,1}{:,3};
+BEN_long = BEN{1,1}{:,4};
+
+UMIDITA_lat = UMIDITA{1,1}{:,2};
+UMIDITA_long = UMIDITA{1,1}{:,3};
+
+PRECIPITAZIONI_lat = PRECIPITAZIONI{1,1}{:,2};
+PRECIPITAZIONI_long = PRECIPITAZIONI{1,1}{:,3};
+
+VEL_VENTO_lat = VEL_VENTO{1,1}{:,2};
+VEL_VENTO_long = VEL_VENTO{1,1}{:,3};
                             
 obj_stem_gridlist_p = stem_gridlist();
 
-ground.coordinates{1} = [NO2_lat, NO2_long];
+ground.coordinates{1} = [NOx_lat, NO2_long];
 ground.coordinates{2} = [traffic_lat, traffic_long];
+ground.coordinates{3} = [PM25_lat, PM25_long];
+ground.coordinates{4} = [BEN_lat, BEN_long];
+ground.coordinates{5} = [UMIDITA_lat, UMIDITA_long];
+ground.coordinates{6} = [PRECIPITAZIONI_lat, PRECIPITAZIONI_long];
+ground.coordinates{7} = [VEL_VENTO_lat, VEL_VENTO_long];
 
 obj_stem_grid1 = stem_grid(ground.coordinates{1}, 'deg', 'sparse', 'point');
 obj_stem_grid2 = stem_grid(ground.coordinates{2}, 'deg', 'sparse', 'point');
+obj_stem_grid3 = stem_grid(ground.coordinates{3}, 'deg', 'sparse', 'point');
+obj_stem_grid4 = stem_grid(ground.coordinates{4}, 'deg', 'sparse', 'point');
+obj_stem_grid5 = stem_grid(ground.coordinates{5}, 'deg', 'sparse', 'point');
+obj_stem_grid6 = stem_grid(ground.coordinates{6}, 'deg', 'sparse', 'point');
+obj_stem_grid7 = stem_grid(ground.coordinates{7}, 'deg', 'sparse', 'point');
+
 obj_stem_gridlist_p.add(obj_stem_grid1);
 obj_stem_gridlist_p.add(obj_stem_grid2);
+obj_stem_gridlist_p.add(obj_stem_grid3);
+obj_stem_gridlist_p.add(obj_stem_grid4);
+obj_stem_gridlist_p.add(obj_stem_grid5);
+obj_stem_gridlist_p.add(obj_stem_grid6);
+obj_stem_gridlist_p.add(obj_stem_grid7);
 
 
 
@@ -341,7 +508,14 @@ shape = [];
 %obj_stem_validation=[];
 S_val1=1:5:n1;
 S_val2=1:5:n2;
-obj_stem_validation=stem_validation({'no2','traffic'},{S_val1,S_val2},0,{'point','point'});
+S_val3=1:5:n3;
+S_val4=1:5:n4;
+S_val5=1:5:n5;
+S_val6=1:5:n6;
+S_val7=1:5:n7;
+
+obj_stem_validation = stem_validation({'nox','traffic','pm25', 'ben', 'umidity', 'prec', 'wind_vel'}, ... 
+    {S_val1,S_val2,S_val3,S_val4,S_val5,S_val6,S_val7},0,{'point','point','point','point','point','point','point'});
 
 obj_stem_modeltype = stem_modeltype('HDGM');
 obj_stem_data = stem_data(obj_stem_varset_p, obj_stem_gridlist_p, ...
@@ -362,10 +536,10 @@ obj_stem_model.stem_data.standardize;
 %Starting values
 obj_stem_par.beta = obj_stem_model.get_beta0();
 obj_stem_par.theta_z = 1;
-obj_stem_par.v_z = [4 2.5; 2.5 2];
-obj_stem_par.sigma_eta = diag([0.2 0.2]);
-obj_stem_par.G = diag([0.9 -0.4]);
-obj_stem_par.sigma_eps = diag([0.02 0.6]);
+obj_stem_par.v_z = eye(7);
+obj_stem_par.sigma_eta = diag([0.02 0.5 0.02 0.1 0.1 0.1 0.1]);
+obj_stem_par.G = diag(0.9*ones(7,1));
+obj_stem_par.sigma_eps = diag([0.02 0.5 0.02 0.02 0.1 0.1 0.1]); 
 
 obj_stem_model.set_initial_values(obj_stem_par);
 
@@ -385,3 +559,4 @@ obj_stem_model.print;
 obj_stem_model.plot_val(0,'no2');
 % validation result for pm2.5
 obj_stem_model.plot_val(0,'traffic');
+save;
